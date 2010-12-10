84eb91e3e1bc15b13e18db86ecaf9d0610f83226
skeen <skeen@cs.au.dk>
2010-12-10 09:31:26 +0000

    typo fix

--- Setup.cmake
+++ Setup.cmake
@@ -1,8 +1,8 @@
 IF(MINGW)
     INCLUDE(${OE_CURRENT_EXTENSION_DIR}/FindWinsock2.cmake)
 
-    IF (NOT BULLET_FOUND) 
+    IF (NOT WINSOCK2_FOUND) 
         MESSAGE ("WARNING: Could not find winsock2 libraries  - depending targets will be disabled.")
-    ENDIF (BULLET_FOUND)
+    ENDIF (NOT WINSOCK2_FOUND)
 ENDIF(MINGW)
 

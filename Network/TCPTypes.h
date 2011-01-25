1155dd149c96cfabefe373e251c6364a04a7816a
norby <norby@genx.dk>
2011-01-25 22:35:25 +0000

    Added general purpose netstatus enum

--- Network/TCPTypes.h
+++ Network/TCPTypes.h
@@ -9,4 +9,17 @@
     typedef int Socket;
 #endif //_WIN32
 
+/*
+ * These status flags are not used directly by the 
+ * current socket implementation but they might be use-full 
+ * for the concrete implementation.
+ */
+typedef enum {
+    NOT_CONNECTED,   // Idle and not connected yet.
+    CONNECTING,      // Connection in progress.
+    CONNECTED,       // Connected successfully and receiving data.
+    CONNECTION_ERR,  // Could not connect.
+    DATA_ERR,        // Connected but there is IO crap.
+} NetStat;
+
 #endif //_SOCKETS_TCP_TYPES_H
